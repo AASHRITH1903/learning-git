5d7c3cb1b17fbd974e6175dbf71ee663d769b58a
Debeshee <debesheedas@gmail.com>
2022-09-12 09:57:06 +0530

    FashionMNIST model added

--- hubconf.py
+++ hubconf.py
@@ -1,52 +1,131 @@
 dependencies = ['torch']
 
 import torch
-import numpy as np
-import math
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+import torch.nn as nn
+from torch import optim
+from torch.autograd import Variable
 
-dtype = torch.float
-device = torch.device("cpu")    
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 def load_data():
 
-    print ('loading data...')
+    train_data = datasets.FashionMNIST(
+    root = 'data',
+    train = True,                         
+    transform = ToTensor(), 
+    download = True,            
+    )
+    test_data = datasets.FashionMNIST(
+        root = 'data', 
+        train = False, 
+        transform = ToTensor()
+    )
+
+    return train_data, test_data
     
-    x = torch.linspace(0, 1, 100, device=device, dtype=dtype)
-    y = 3 + 4 * x + 5 * x**2 + 6 * x**3
+def get_dataloaders(train_data, test_data):
+    loaders = {
+    'train' : torch.utils.data.DataLoader(train_data, 
+                                          batch_size=100, 
+                                          shuffle=True, 
+                                          num_workers=1),
+    
+    'test'  : torch.utils.data.DataLoader(test_data, 
+                                          batch_size=100, 
+                                          shuffle=True, 
+                                          num_workers=1),
+    }
+    return loaders
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(         
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=16,            
+                kernel_size=5,              
+                stride=1,                   
+                padding=2,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2),    
+        )
+        self.conv2 = nn.Sequential(         
+            nn.Conv2d(16, 32, 5, 1, 2),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),                
+        )
+        # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 7 * 7, 10)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)       
+        output = self.out(x)
+        return output, x    # return x for visualization
+
+def get_model():
+    cnn = CNN()
+    loss_func = nn.CrossEntropyLoss()   
+    optimizer = optim.Adam(cnn.parameters(), lr = 0.01)   
+    return cnn, loss_func, optimizer
+
+    
+
+def train(cnn, loss_func, optimizer, loaders):
+    num_epochs = 10
+    cnn.train()
         
-    return x, y
-    
-    
-def polyfit_autograd(x,y):
-
-    a = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    b = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    c = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    d = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-
-    learning_rate = 1e-4
-
-    # 2022 Aug 23 - Kalidas (ykalidas@iittp.ac.in)
-    optimizer = torch.optim.SGD([a,b,c,d], lr=learning_rate, momentum=0)
-    
-    for t in range(10000):
-    
-        # Forward pass: compute predicted y using operations on Tensors.
-        y_pred = a + b * x + c * x ** 2 + d * x ** 3
-
-        # Compute and print loss using operations on Tensors.
-        # Now loss is a Tensor of shape (1,)
-        # loss.item() gets the scalar value held in the loss.
-        loss = (y_pred - y).pow(2).sum()
-
-        # 2022 Aug 23 - Kalidas (ykalidas@iittp.ac.in)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if t % 1000 == 99:
-            print(t, loss.item())    
+    # Train the model
+    total_step = len(loaders['train'])
         
-    print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(loaders['train']):
+            
+            # gives batch data, normalize x when iterate train_loader
+            b_x = Variable(images)   # batch x
+            b_y = Variable(labels)   # batch y
+            output = cnn(b_x)[0]               
+            loss = loss_func(output, b_y)
+            
+            # clear gradients for this training step   
+            optimizer.zero_grad()           
+            
+            # backpropagation, compute gradients 
+            loss.backward()    
+            # apply gradients             
+            optimizer.step()                
+            
+            if (i+1) % 100 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                       .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+            pass
         
-    return a, b, c, d
+        pass
+    
+    
+    pass
+
+def test(cnn, loaders):
+    # Test the model
+    cnn.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in loaders['test']:
+            test_output, last_layer = cnn(images)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+            pass
+        print('Test Accuracy of the model on the 10000 test images: %.2f' % accuracy)
+    
+        pass
+
